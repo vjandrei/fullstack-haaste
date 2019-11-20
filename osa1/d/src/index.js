61de6f0b692bd227712dc0dc5524499526a221d8
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2019-11-20 11:50:55 +0200

    fiksumpi handleclick

--- osa1/d/src/index.js
+++ osa1/d/src/index.js
@@ -10,26 +10,34 @@ const Button = ({handleClick, name}) => {
 }
 
 const App = () => {
+
+  const goodText = "good"
+  const neutralText = "neutral"
+  const badText = "bad" 
+
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const goodText = "good"
-  const neutralText = "neutral"
-  const badText = "bad"
+  const clickGood = () => { setGood(good + 1)}
+  const clickNeutral = () => { setNeutral(neutral + 1)}
+  const clickDad = () => { setBad(bad + 1)}
+  
+  
 
   return (
     <div>
         <h1>Give feedback</h1>
-        <Button handleClick={() => setGood(good + 1)}  name={goodText} />
-        <Button handleClick={() => setNeutral(neutral + 1)}  name={neutralText} />
-        <Button handleClick={() => setBad(bad + 1)}  name={badText} />
+        <Button handleClick={clickGood}  name={goodText} />
+        <Button handleClick={clickNeutral}  name={neutralText} />
+        <Button handleClick={clickDad}  name={badText} />
         <h1>Statistic</h1>
         <ul>
             <li>Good: {good}</li>
             <li>Neutral: {neutral}</li>
             <li>Dad: {bad}</li>
+            <li>All: </li>
         </ul>
     </div>
   )
